3de9cb5fe406d831af6626d1ac7222943e7662c7
rachel551 <r.python551@gmail.com>
2024-09-02 03:22:35 -0400

    add github workflow

--- xero_backend/tests/test_application.py
+++ xero_backend/tests/test_application.py
@@ -8,6 +8,10 @@ root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
 
 from application.app import XeroApp
+from services.balance_sheet_service import BalanceSheetService
+
+
+MOCK_URL = 'http://fakeurl.com/api.xro/2.0/Reports/BalanceSheet'
 
 @pytest.fixture
 def app():
@@ -17,13 +21,14 @@ def app():
 def client(app):
     return app.test_client()
 
-@patch('xero_app.BalanceSheetService')
-@patch('xero_app.Config.load_config')
+@patch('services.balance_sheet_service.BalanceSheetService')
+@patch('application.app.Config.load_config')
 def test_get_balance_sheet_success(mock_load_config, mock_balance_sheet_service, client):
-    # Setup mocks
-    mock_load_config.return_value = (MagicMock(), '0.0.0.0', 5000)
+    mock_logger = MagicMock()
+    mock_load_config.return_value = (mock_logger, '0.0.0.0', 5000, MOCK_URL)
+
     mock_balance_sheet_service_instance = MagicMock()
-    mock_balance_sheet_service_instance.get_balance_sheet_data.return_value = {'status': 'success'}
+    mock_balance_sheet_service_instance.get_balance_sheet_data.return_value = {'Reports': [{'ReportID': '1', 'Status': 'OK'}]}
     mock_balance_sheet_service.return_value = mock_balance_sheet_service_instance
 
     response = client.get('/api/balance-sheet')
@@ -32,11 +37,13 @@ def test_get_balance_sheet_success(mock_load_config, mock_balance_sheet_service,
     assert response.json == {'status': 'success'}
     mock_balance_sheet_service_instance.get_balance_sheet_data.assert_called_once()
 
-@patch('xero_app.BalanceSheetService')
-@patch('xero_app.Config.load_config')
+
+@patch('services.balance_sheet_service.BalanceSheetService')
+@patch('application.app.Config.load_config')
 def test_get_balance_sheet_failure(mock_load_config, mock_balance_sheet_service, client):
-    # Setup mocks
-    mock_load_config.return_value = (MagicMock(), '0.0.0.0', 5000)
+    mock_logger = MagicMock()
+    mock_load_config.return_value = (mock_logger, '0.0.0.0', 5000, MOCK_URL)
+
     mock_balance_sheet_service_instance = MagicMock()
     mock_balance_sheet_service_instance.get_balance_sheet_data.side_effect = Exception("Service error")
     mock_balance_sheet_service.return_value = mock_balance_sheet_service_instance
--- xero_backend/tests/test_services.py
+++ xero_backend/tests/test_services.py
@@ -8,35 +8,39 @@ root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
 
 from services.balance_sheet_service import BalanceSheetService
+from application.app import Config 
 
+MOCK_URL = 'http://fakeurl.com/api.xro/2.0/Reports/BalanceSheet'
 
 @patch('services.balance_sheet_service.requests.get')
-@patch('services.balance_sheet_service.os.getenv')
-def test_get_balance_sheet_data_success(mock_getenv, mock_get):
-    mock_getenv.return_value = 'http://fakeurl.com/api.xro/2.0/Reports/BalanceSheet'
-
-    # Mock the response of requests.get
+@patch('application.app.Config.load_config')
+def test_get_balance_sheet_data_success(mock_load_config, mock_get):
+    mock_logger = MagicMock()
+    mock_load_config.return_value = (mock_logger, 'localhost', 5000, MOCK_URL)
     mock_response = MagicMock()
     mock_response.raise_for_status = MagicMock()
     mock_response.json.return_value = {'status': 'success'}
     mock_get.return_value = mock_response
 
-    service = BalanceSheetService()
+    service = BalanceSheetService(base_url=MOCK_URL)  # Pass the mock URL directly
     result = service.get_balance_sheet_data()
 
     assert result == {'status': 'success'}
-    mock_get.assert_called_once_with('http://fakeurl.com/api.xro/2.0/Reports/BalanceSheet')
+    mock_get.assert_called_once_with(MOCK_URL)
     mock_response.raise_for_status.assert_called_once()
 
 
 @patch('services.balance_sheet_service.requests.get')
-@patch('services.balance_sheet_service.os.getenv')
-def test_get_balance_sheet_data_failure(mock_getenv, mock_get):
-    mock_getenv.return_value = 'http://fakeurl.com/api.xro/2.0/Reports/BalanceSheet'
+@patch('application.app.Config.load_config')
+def test_get_balance_sheet_data_failure(mock_load_config, mock_get):
+    mock_logger = MagicMock()
+    mock_load_config.return_value = (mock_logger, 'localhost', 5000, MOCK_URL)
+
     mock_get.side_effect = requests.exceptions.RequestException("Network error")
-    service = BalanceSheetService()
+
+    service = BalanceSheetService(base_url=MOCK_URL)
     with pytest.raises(Exception) as excinfo:
         service.get_balance_sheet_data()
 
-    assert 'Failed to fetch balance sheet data: Network error' in str(excinfo.value)
-    mock_get.assert_called_once_with('http://fakeurl.com/api.xro/2.0/Reports/BalanceSheet')
+    assert 'An error occurred while fetching balance sheet data: Network error' in str(excinfo.value)
+    mock_get.assert_called_once_with(MOCK_URL)
